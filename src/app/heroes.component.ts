eb2ea84998cbd95fccb4aa0c0d19ffb439e046e2
smile9x <smile9x@gmail.com>
2017-10-10 14:32:30 +0700

    add http

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -44,11 +44,20 @@ export class HeroesComponent implements OnInit {
 
   add(name: string): void {
     name = name.trim();
-    if (!name) return;
+    if (!name) {return;}
     this.heroService.create(name)
             .then(hero => {
               this.heroes.push(hero);
               this.selectedHero = null;
             });
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+        .delete(hero.id)
+        .then(() => {
+          this.heroes = this.heroes.filter(h => h !== hero);
+          if (this.selectedHero === hero) { this.selectedHero = null; }
+        });
   }
 }
